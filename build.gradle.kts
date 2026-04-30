0aa7331a383fcf8be6a5fc85c0242ebe02abbaf3
Paweł Ryfiak <pawel.ryfiak@student.kdg.be>
2026-04-30 17:14:01 +0200

    test: switch test profile from H2 to PostgreSQL with CI_DB_HOST_PORT variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("com.h2database:h2")
+
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
